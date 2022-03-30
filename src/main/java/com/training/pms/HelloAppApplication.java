0c529b3d3b7fac5f3bb222d4f4b37cf8032ccf92
wenjian206 <wenjian206@revature.net>
2022-03-30 10:10:01 -0700

    first commit

---
+++ src/main/java/com/training/pms/HelloAppApplication.java
@@ -0,0 +1,20 @@
+package com.training.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloAppApplication.class, args);
+	}
+	@GetMapping("/sayHello")
+	public String sayHello() {
+		return "Hello My Friend";
+	}
+
+}
